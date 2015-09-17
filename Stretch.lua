8187c5f3f5a7aace659a14650508971ae80e912c
viktorx11 <viktorx11@gmail.com>
2015-09-17 06:37:31 +0200

    update wheel and stretch

--- Stretch.lua
+++ Stretch.lua
@@ -3,19 +3,18 @@ return function(page, offset, screen_width, screen_height)
   local percent = math.abs(offset/page.width)
 
   for i, icon in subviews(page) do
-    -- icon center
     local icon_centerx = icon.x + icon.width/2
     local icon_offsetx =  math.abs((page.width/2)-icon_centerx)
+	local percent_pright = (icon_centerx) / page.width
+    local percent_pleft = (page.width-icon_centerx) / page.width
 
-    if offset < 0 then -- right page
-      local percent2 = (icon_centerx+50) / page.width
-      icon:translate(icon_centerx*percent*percent2*1.5, 0, 0)
-      --icon:scale(1+(percent*percent2)*2, 1)
+	if offset < 0 then -- right page
+      icon:translate(icon_centerx*percent*percent_pright*4, 0, 0)
+      icon:scale(1+(percent*percent*percent_pright/percent_pright), 1)
     else -- left page
-      local percent2 = (page.width-icon_centerx) / page.width
-      icon:translate(-((page.width-icon_centerx)*percent*percent2)*1.5, 0, 0)
-      --icon:scale(1+(percent*percent2)*2, 1)
+      icon:translate(-((page.width-icon_centerx)*percent*percent_pleft)*4, 0, 0)
+      icon:scale(1+(percent*percent*percent_pleft/percent_pleft), 1)
     end
   end
-  page.layer.x = page.layer.x - offset * 1.2
+  page.layer.x = page.layer.x - (offset * percent *3)
 end
--- Wheel.lua
+++ Wheel.lua
@@ -6,7 +6,7 @@ return function(page, offset, screen_width, screen_height)
     local icon_centerx = icon.x + icon.width/2
     local icon_centery = icon.y + icon.height/2
     local icon_center_offsetx = (page.width/2)-icon_centerx
-    local radius4icon = screen_height - icon_centery + 50
+    local radius4icon = screen_height - icon_centery + 0
 
     local percent2
     if offset < 0 then -- right page
@@ -15,10 +15,10 @@ return function(page, offset, screen_width, screen_height)
       percent2 = ((page.width-icon_centerx) / page.width)
     end
 
-    local angle = -percent*(1 + percent2 *2) *  math.pi/2
-
+    local angle = -percent*(1 + percent2*2) *  math.pi/2
+    --angle = angle * (percent*percent*2)
     icon:translate(icon_center_offsetx, radius4icon, 0)
     icon:rotate(angle, 0, 0, 1)
     icon:translate(-icon_center_offsetx, -radius4icon, 0)
-  end  
+  end
 end
